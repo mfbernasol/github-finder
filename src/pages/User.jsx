d00d3830ddb0019c62d37a09447bea8e1a8be7fa
mfbernasol <mfbernasol@gmail.com>
2023-03-14 14:19:34 -0500

    refactor with axios

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -6,7 +6,7 @@ import RepoList from '../components/repos/RepoList';
 import { useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import GithubContext from '../context/github/GithubContext';
-import { getUser, getUserRepos } from '../context/github/GithubActions';
+import {  getUserAndRepos} from '../context/github/GithubActions';
 
 function User() {
   const { user, loading, repos, dispatch } = useContext(GithubContext);
@@ -15,11 +15,8 @@ function User() {
 
   useEffect(() => {
     const getUserData = async () => {
-      const userData = await getUser(params.login);
-      dispatch({ type: 'GET_USER', payload: userData });
-
-      const userRepoData = await getUserRepos(params.login);
-      dispatch({ type: 'GET_REPOS', payload: userRepoData });
+      const userData = await getUserAndRepos(params.login);
+      dispatch({ type: 'GET_USER_AND_REPOS', payload: userData });
     };
 
     getUserData();
